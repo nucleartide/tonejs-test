0cfd908201064e7d29d7147aae52df810fadc690
Jason Tu <jasontu4@gmail.com>
2019-09-14 20:31:45 -0700

    this is cool

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log('hello')
-console.log('hello')
+
+// Set up Tone.js:
+document.querySelector('button').addEventListener('click', () => {
+  Tone.start()
+  main()
+})
+
+function main() {
+  // Create a synth and connect it to the master output (laptop speakers):
+  const synth = new Tone.Synth()
+    .toMaster()
+
+  // Play a middle 'C' for the duration of an 8th note:
+  synth.triggerAttackRelease('C4', '8n')
+}
